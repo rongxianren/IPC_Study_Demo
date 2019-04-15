1077df06da20dcc9a64f76347094d62207dd06cf
wangtangyun <wangtangyun@onething.net>
2019-04-15 16:12:59 +0800

    利用aidl进行ipc 通信

---
+++ app/src/main/java/com/rongxianren/ipc/aidl/BookManagerService.java
@@ -0,0 +1,45 @@
+package com.rongxianren.ipc.aidl;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class BookManagerService extends Service {
+
+
+    private CopyOnWriteArrayList<Book> mBookList = new CopyOnWriteArrayList();
+
+    private Binder mBinder = new IBookManager.Stub() {
+        @Override
+        public List<Book> getBookList() throws RemoteException {
+            return mBookList;
+        }
+
+        @Override
+        public void addBook(Book book) throws RemoteException {
+            mBookList.add(book);
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mBookList.add(new Book(1001, "乡土中国"));
+        mBookList.add(new Book(1002, "怪诞行为学"));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+}
